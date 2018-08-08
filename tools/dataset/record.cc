7f6effebbbde31b2d0d81fc19af4227609d227c2
Kalman <kalmanxiong@slightech.com>
2018-08-09 05:46:27 +0800

    Delete 't' in record.cc

--- tools/dataset/record.cc
+++ tools/dataset/record.cc
@@ -106,10 +106,10 @@ int main(int argc, char *argv[]) {
       cv::Mat left_img(
           left_frame->height(), left_frame->width(), CV_8UC3,
           left_frame->data());
-      cv::cvtColor(left_img, left_img, CV_BGR2RGB);
       cv::Mat right_img(
           right_frame->height(), right_frame->width(), CV_8UC3,
           right_frame->data());
+      cv::cvtColor(left_img, left_img, CV_BGR2RGB);
       cv::cvtColor(right_img, right_img, CV_BGR2RGB);
       cv::hconcat(left_img, right_img, img);
     } else {
