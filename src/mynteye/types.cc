46903bb10f30a98c16b48508473b9eb569b72253
Osenberg <junyangqu@slightech.com>
2019-03-30 18:21:55 +0800

    fix(src): imu params assembly show error

--- src/mynteye/types.cc
+++ src/mynteye/types.cc
@@ -252,7 +252,7 @@ std::ostream &operator<<(std::ostream &os, const ImuIntrinsics &in) {
     os << in.assembly[0][i] << ", ";
   for (int i = 0; i <= 2; i++)
     os << in.assembly[1][i] << ", ";
-  for (int i = 0; i <= 2; i++)
+  for (int i = 0; i <= 1; i++)
     os << in.assembly[2][i] << ", ";
   os << in.assembly[2][2] << "]";
 
