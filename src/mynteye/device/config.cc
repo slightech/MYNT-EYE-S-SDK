559b1e4d807f61b0bcab9dabc1648955e273712e
TinyO <tinysilk@hotmail.com>
2019-08-19 09:45:10 +0800

    fix(windows): runtime core.

--- src/mynteye/device/config.cc
+++ src/mynteye/device/config.cc
@@ -70,8 +70,8 @@ const std::map<Model, std::map<Capabilities, StreamRequests>>
 stream_requests_map = {
   {Model::STANDARD,
     {{Capabilities::STEREO, {
-      {752, 480, Format::YUYV, 0},
-      {376, 240, Format::YUYV, 0}}
+      {752, 480, Format::YUYV, 60},
+      {376, 240, Format::YUYV, 60}}
     }}
   },
   {Model::STANDARD2,
