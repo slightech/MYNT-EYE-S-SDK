db572a6b90797bc345dbcc1412dfc788ed232862
TinyO <tinysilk@hotmail.com>
2019-10-21 17:24:10 +0800

    fix(*): windows complie error 2.

--- src/mynteye/device/streams.cc
+++ src/mynteye/device/streams.cc
@@ -59,7 +59,7 @@ void Streams::CheckTimeStampLimmit(std::shared_ptr<ImgData> img) {
   }
   if (is_nearly_before_timestamp_limmit_ > 0) {
     is_nearly_before_timestamp_limmit_--;
-    if (abs(current_datum_ - img->timestamp) > (uint64_t)(42949672960/2)) {  // NOLINT
+    if (fabs(current_datum_ - img->timestamp) > (uint64_t)(42949672960/2)) {  // NOLINT
       img->timestamp -= 42949672960;
     }
   }
